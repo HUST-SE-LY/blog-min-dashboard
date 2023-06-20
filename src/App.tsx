b98963f74da550d97032e43f8758d604109552f0
cheems1969 <cheems1969@gmail.com>
2023-06-20 16:41:45 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { lazy, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import dashBoardConfig from "./blogDashboard";
 import Blog from "./components/Blog";
 import { observer } from "mobx-react-lite";
